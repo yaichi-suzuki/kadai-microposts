662616a0eb8d6bddf5aa8911ef7a7818ca74b4b7
yaichi.suzuki <proappler@gmail.com>
2020-04-17 05:48:02 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }  #レコードセーブ前にself(Userインスタンス)のemailをdowncase(全て小文字にする)　！は自分自身を直接変換すること
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, #正しいメールアドレス記法かどうか
+                    uniqueness: { case_sensitive: false } #uniqueness: 重複許さない　case~大文字小文字を区別しない
+  has_secure_password
+end
