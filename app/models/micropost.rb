82c8c787acc10302fb94e30341ec001a41effcd8
yaichi.suzuki <proappler@gmail.com>
2020-05-11 13:53:20 +0000

    add Favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites
+  has_many :users_who_has_favorite, through: :favorites, source: :user
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
